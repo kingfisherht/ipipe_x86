1b5807cd62ce707a73b5715f4b8209e6c3561a2a
zhang,qianyi <qianyi@qianyi.com>
2020-04-28 11:17:25 +0800

    ipipe-core-4.19.89-x86-9

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#4 SMP Tue Apr 28 09:13:03 CST 2020"
+#define LINUX_COMPILE_BY "intel"
+#define LINUX_COMPILE_HOST "intel-NUC"
+#define LINUX_COMPILER "gcc version 7.5.0 (Ubuntu 7.5.0-3ubuntu1~18.04)"
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.19.75-xenomai-3.0.7+"
